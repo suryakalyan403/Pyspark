ccb78546c4830b1ce0bbf5093a33f26137a34f4f
Surya Kalyan Jaddu <99590270+suryakalyan403@users.noreply.github.com>
2023-03-12 14:10:35 +0530

    Delete pyspark.py

--- Spark-note-books/pyspark.py
+++
@@ -1,6 +0,0 @@
-"""val w = Window.orderBy($"SL_NO")
-dataset.withColumn("AMOUNT",
-         when($"ID" === lag($"ID", 1).over(w),
-              lag($"AMOUNT", 1).over(w))\
-              .otherwise($"AMOUNT")
-).show"""
